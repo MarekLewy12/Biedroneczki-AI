5860180e3a2681d6a32aae869c31883ebd3a7f72
kapikoks1 <kacpeer1k@gmail.com>
2024-12-28 00:06:05 +0100

    font-increase

--- js/app.js
+++ js/app.js
@@ -16,3 +16,26 @@ const toggleDarkMode = () => {
 };
 
 darkModeToggle.addEventListener('click', toggleDarkMode);
+
+
+
+
+//rozmiar czcionki (50,75,100,125%)
+const fontIncreaseButton = document.getElementById('fontIncreaseButton');
+
+
+let scaleIndex = 4; 
+const scaleSteps = [0.5, 0.75, 1, 1.25]; 
+
+const changePageScale = () => {
+  
+  scaleIndex = (scaleIndex + 1) % scaleSteps.length; 
+  const newScale = scaleSteps[scaleIndex]; 
+
+  
+  document.body.style.transform = `scale(${newScale})`; 
+  document.body.style.transformOrigin = 'top left'; 
+  document.body.style.transition = 'transform 0.3s ease'; 
+};
+
+fontIncreaseButton.addEventListener('click', changePageScale);
