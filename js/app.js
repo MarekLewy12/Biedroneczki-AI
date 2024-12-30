2c554e3a2eef1591a5f57acf61003503a20d2be3
MarekLewy12 <lewarmarko@gmail.com>
2024-12-30 14:21:02 +0100

    fix font change

--- js/app.js
+++ js/app.js
@@ -3,15 +3,15 @@ const darkStyleLink = document.getElementById('darkStyle');
 
 // Funkcja przełączająca tryb nocny
 const toggleDarkMode = () => {
-  
+
   if (darkStyleLink.disabled) {
     darkStyleLink.disabled = false; // Włącz tryb ciemny
-    darkModeToggle.src = 'img/light-mode.png'; 
-    darkModeToggle.alt = 'Przycisk do zmiany trybu dziennego'; 
+    darkModeToggle.src = 'img/light-mode.png';
+    darkModeToggle.alt = 'Przycisk do zmiany trybu dziennego';
   } else {
     darkStyleLink.disabled = true; // Wyłącz tryb ciemny
-    darkModeToggle.src = 'img/dark-mode.png'; 
-    darkModeToggle.alt = 'Przycisk do zmiany trybu nocnego'; 
+    darkModeToggle.src = 'img/dark-mode.png';
+    darkModeToggle.alt = 'Przycisk do zmiany trybu nocnego';
   }
 };
 
@@ -20,22 +20,26 @@ darkModeToggle.addEventListener('click', toggleDarkMode);
 
 
 
-//rozmiar czcionki (50,75,100,125%)
+// Rozmiar czcionki (50%, 75%, 100%, 125%)
 const fontIncreaseButton = document.getElementById('fontIncreaseButton');
 
+// Informacja o aktualnej skali czcionki
+const fontScaleInfo = document.getElementById('fontScaleInfo');
 
-let scaleIndex = 4; 
-const scaleSteps = [0.5, 0.75, 1, 1.25]; 
+let scaleIndex = 2;  // Strona po załadowaniu ma 100% skali
+const scaleSteps = [0.5, 0.75, 1, 1.25];
 
-const changePageScale = () => {
-  
-  scaleIndex = (scaleIndex + 1) % scaleSteps.length; 
-  const newScale = scaleSteps[scaleIndex]; 
+const changePageFontScale = () => {
 
-  
-  document.body.style.transform = `scale(${newScale})`; 
-  document.body.style.transformOrigin = 'top left'; 
-  document.body.style.transition = 'transform 0.3s ease'; 
+  scaleIndex = (scaleIndex + 1) % scaleSteps.length;
+  const newScale = scaleSteps[scaleIndex];
+
+  // Zmiana rozmiaru czcionki na stronie
+  document.documentElement.style.fontSize = `${newScale}em`;
+
+  // Aktualizacja informacji o skali czcionki
+  fontScaleInfo.textContent = `${newScale * 100}%`;
+
 };
 
-fontIncreaseButton.addEventListener('click', changePageScale);
+fontIncreaseButton.addEventListener('click', changePageFontScale);
